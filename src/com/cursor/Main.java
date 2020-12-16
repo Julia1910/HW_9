48be22dcfa8086168c55ebc8a9932fcaa2eb615f
Juliya <ulcakopitko@gmail.com>
2020-12-16 20:14:04 +0200

    Add throws Error

--- src/com/cursor/Main.java
+++ src/com/cursor/Main.java
@@ -1,8 +1,25 @@
 package com.cursor;
 
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+
 public class Main {
 
-    public static void main(String[] args) {
-	// write your code here
+    public static void main(String[] args) throws Exception {
+        System.out.println("~Start~");
+        String string = "STR";
+        while (true) {
+            File file = new File("file.txt");
+
+            BufferedReader br = new BufferedReader(new FileReader(file));
+
+            String st;
+            while ((st = br.readLine()) != null) {
+                string += string + st;
+            }
+            System.out.println(string);
+        }
     }
 }
